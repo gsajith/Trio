90dcf9da4c7d45bbd6f5c232d36f8ec09d1eeb5b
gsajith <gsajith@umich.edu>
2014-10-03 17:31:56 -0400

    Full bleed list view, click listener for list items

--- TrioApp/app/src/main/java/com/trioshows/trio/ListViewFragment.java
+++ TrioApp/app/src/main/java/com/trioshows/trio/ListViewFragment.java
@@ -1,22 +1,64 @@
 package com.trioshows.trio;
 
+import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListViewFragment extends Fragment {
-    public ListViewFragment(){}
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.volley.adapter.CustomListAdapter;
+import com.volley.app.AppController;
+import com.volley.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewFragment extends ListFragment {
+    // Log tag
+    private static final String TAG = ListViewFragment.class.getSimpleName();
+    // Movies json url
+    private static final String url = "http://api.androidhive.info/json/movies.json";
+    private ProgressDialog pDialog;
+    private List<Movie> movieList = new ArrayList<Movie>();
+    private ListView listView;
+    private CustomListAdapter adapter;
+
+    public ListViewFragment() {
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(getActivity(), "You clicked something at " + position + " and id " + id,
+                Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.list_fragment, container, false);
-        WebView myWebView = (WebView) rootView.findViewById(R.id.webview);
+        /*WebView myWebView = (WebView) rootView.findViewById(R.id.webview);
         myWebView.loadUrl("http://www.trioshows.com");
 
         myWebView.setWebViewClient(new WebViewClient() {
@@ -25,7 +67,83 @@ public class ListViewFragment extends Fragment {
                 view.loadUrl(url);
                 return true;
             }
+        });*/
+
+        listView = (ListView) rootView.findViewById(android.R.id.list);
+
+        adapter = new CustomListAdapter(this.getActivity(), movieList);
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(this.getActivity());
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        // Creating volley request obj
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Movie movie = new Movie();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("image"));
+                                movie.setRating(((Number) obj.get("rating"))
+                                        .doubleValue());
+                                movie.setYear(obj.getInt("releaseYear"));
+
+                                // Genre is json array
+                                JSONArray genreArry = obj.getJSONArray("genre");
+                                ArrayList<String> genre = new ArrayList<String>();
+                                for (int j = 0; j < genreArry.length(); j++) {
+                                    genre.add((String) genreArry.get(j));
+                                }
+                                movie.setGenre(genre);
+
+                                // adding movie to movies array
+                                movieList.add(movie);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
         });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);
         return rootView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
     }
 }
